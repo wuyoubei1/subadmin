b0b08da217aa5b6314b79c6ce44c64f239c0f62e
wuyb <389234613@qq.com>
2017-04-19 17:39:27 +0800

    1

--- subadmin/src/main/java/com/thinkgem/jeesite/modules/sub/web/front/PProductTbFrontController.java
+++ subadmin/src/main/java/com/thinkgem/jeesite/modules/sub/web/front/PProductTbFrontController.java
@@ -6,25 +6,16 @@ package com.thinkgem.jeesite.modules.sub.web.front;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-import org.apache.shiro.authz.annotation.RequiresPermissions;
-import org.junit.runners.Parameterized.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.thinkgem.jeesite.common.config.Global;
-import com.thinkgem.jeesite.common.persistence.Page;
 import com.thinkgem.jeesite.common.web.BaseController;
-import com.thinkgem.jeesite.common.utils.StringUtils;
 import com.thinkgem.jeesite.modules.sub.entity.PProductTb;
 import com.thinkgem.jeesite.modules.sub.service.PProductTbService;
 
