956d97972e312b624236880d2059fe18447ffb6f
wuyb <389234613@qq.com>
2017-05-13 13:11:19 +0800

    12

---
+++ subadmin/src/main/java/com/thinkgem/jeesite/modules/sub/dao/YouHuiQuanDao.java
@@ -0,0 +1,24 @@
+package com.thinkgem.jeesite.modules.sub.dao;
+
+import java.util.List;
+
+import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
+import com.thinkgem.jeesite.modules.sub.entity.LimitEntity;
+import com.thinkgem.jeesite.modules.sub.entity.YouHuiQuan;
+
+/**
+* 
+* @author xingyu
+* @version 2017年5月5日 
+*/
+
+@MyBatisDao
+public interface YouHuiQuanDao {
+
+	public List<YouHuiQuan> getByGoodsId(String goodsId);
+	
+	/***获取列表数据***/
+	public List<YouHuiQuan> getList(int page);
+	
+	public List<YouHuiQuan> getListByTitle(LimitEntity limit);
+}
