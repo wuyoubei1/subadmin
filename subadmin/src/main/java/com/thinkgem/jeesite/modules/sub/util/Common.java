ed03a8edb06cc3ede3b44814b5abd0b48e1d14ec
wuyb <389234613@qq.com>
2017-06-03 23:25:58 +0800

    增加公共类

---
+++ subadmin/src/main/java/com/thinkgem/jeesite/modules/sub/util/Common.java
@@ -0,0 +1,14 @@
+package com.thinkgem.jeesite.modules.sub.util;
+
+public class Common {
+
+public static String url="http://gw.api.taobao.com/router/rest"; 
+	
+	public static String appkey="23760845";
+	
+	public static String secret="e5cf5879a13d0c453c3bb1a9bff4b861";
+	
+	public static Long adzoneId=80460157L;
+	
+	public static String pid="mm_10221473_23986300_80460157";
+}
